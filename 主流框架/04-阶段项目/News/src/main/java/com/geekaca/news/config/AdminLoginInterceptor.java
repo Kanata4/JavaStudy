23bc9410bef258bc5fd11712d81029b4a4f6e1e4
liaohao <whitealbum17@outlook.com>
2023-09-19 04:35:54 +0800

    增加拦截器

---
+++ 主流框架/04-阶段项目/News/src/main/java/com/geekaca/news/config/AdminLoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.geekaca.news.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 拦截器
+ * 保护后台的接口
+ */
+@Component
+public class AdminLoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
+        String requestServletPath = request.getServletPath();
+        if (requestServletPath.startsWith("/admin") && null == request.getSession().getAttribute("loginUser")) {
+            request.getSession().setAttribute("errorMsg", "请重新登陆");
+            response.sendRedirect(request.getContextPath() + "/admin/login");
+            return false;
+        } else {
+            request.getSession().removeAttribute("errorMsg");
+            return true;
+        }
+        
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
